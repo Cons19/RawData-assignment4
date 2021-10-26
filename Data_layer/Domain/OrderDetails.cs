9ea07572e1edbd6b481568d1042dbe6abc476066
Constantin <razvan1598@gmail.com>
2021-10-26 15:09:30 +0200

    added mapping to the entity framework

--- Data_layer/Domain/OrderDetails.cs
+++ Data_layer/Domain/OrderDetails.cs
@@ -9,6 +9,7 @@ namespace Data_layer.Domain
     public class OrderDetails
     {
         public int Id { get; set; }
+        public int ProductId { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
         public int Discount { get; set; }
--- Data_layer/NorthwindContext.cs
+++ Data_layer/NorthwindContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Data_layer.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace Data_layer
 {
     public class NorthwindContext : DbContext
     {
+        // change these variables depending on your local machine
+        private string uid = "postgres";
+        private string password = "pass123";
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
@@ -16,25 +20,47 @@ namespace Data_layer
         {
             base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
-            optionsBuilder.EnableSensitiveDataLogging();
-            optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=bulskov;pwd=henrik");
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            //optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseNpgsql($"host=localhost;db=northwind;uid={uid};pwd={password}");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // category mapping
             modelBuilder.Entity<Category>().ToTable("categories");
             modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
             modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
             modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
 
-
+            // product mapping
             modelBuilder.Entity<Product>().ToTable("products");
             modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
             modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
             modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
+            modelBuilder.Entity<Product>().Property(x => x.Quantity).HasColumnName("quantityperunit");
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasColumnName("unitprice");
+            modelBuilder.Entity<Product>().Property(x => x.Stock).HasColumnName("unitsinstock");
+
+            // order details
+            modelBuilder.Entity<OrderDetails>().ToTable("orderdetails");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.Id).HasColumnName("orderid");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.ProductId).HasColumnName("productid");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.Price).HasColumnName("unitprice");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.Quantity).HasColumnName("quantity");
+            modelBuilder.Entity<OrderDetails>().Property(x => x.Discount).HasColumnName("discount");
+
+            // order
+            modelBuilder.Entity<Order>().ToTable("orders");
+            modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+            modelBuilder.Entity<Order>().Property(x => x.Date).HasColumnName("orderdate");
+            modelBuilder.Entity<Order>().Property(x => x.Require).HasColumnName("requireddate");
+            modelBuilder.Entity<Order>().Property(x => x.Shipped).HasColumnName("shippeddate");
+            modelBuilder.Entity<Order>().Property(x => x.Freight).HasColumnName("freight");
+            modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname");
+            modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
 
         }
     }
